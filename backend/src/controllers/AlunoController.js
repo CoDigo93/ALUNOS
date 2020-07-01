4a3fcfa083f3dba9f02e00df952540eb78fe6f53
Wagner Lustosa <git@github.com:CoDigo93/ALUNOS.git>
2020-07-01 01:02:55 -0300

    inserção de função pra atualização parcial de um aluno

--- backend/src/controllers/AlunoController.js
+++ backend/src/controllers/AlunoController.js
@@ -1,12 +1,17 @@
-Aluno = require('../models/Aluno');
+const Aluno = require('../models/Aluno');
+
+
 
 module.exports = {
     async index(request, response){
         const alunos = await Aluno.find();
+        if(!alunos) return response.status(400).send('Não foi encontrado nenhum aluno.');
+
         return response.json(alunos);
     },
 
     async store(request, response){
+        try{
         const {name, instrument} = request.body;
         
         let aluno = await Aluno.findOne({name});
@@ -20,10 +25,16 @@ module.exports = {
             
             return response.json(alunos);
         }
+            
+        }catch(err){
+            return response.status(400).send('Erro na criação, tente novamente!');
+        }
     }, 
 
     async delete(request,response){
         const delaluno = await Aluno.deleteMany(request.params);
+        if(!delaluno) return response.status(400).send('Não foi encontrado registros com esse id!');
+
         console.log(request.params);
         console.log(delaluno);
         return response.json(delaluno);
@@ -33,6 +44,7 @@ module.exports = {
     async update(request,response){
         
         console.log(request.body);
+        try{
 
         const upAluno = await Aluno.findByIdAndUpdate(request.params._id,
             {$set:{name:request.body.name,
@@ -42,8 +54,46 @@ module.exports = {
                     useFindAndModify:false
         
         });
-            
+        
         console.log(upAluno);
         return response.json(upAluno);
+    }catch(err){
+        return response.status(400).send('Não foi possível atualizar o registro,tente novamente!');
     }
-}
+    },
+
+    async parcialUpdate(request,response){
+        console.log('chamando a parcialUpdate');
+        try{
+        const aluno = await Aluno.findByIdAndUpdate(request.params._id, request.body, {
+            new:true,
+            useFindAndModify: false
+        });
+        
+        
+        if(!aluno) return response.status(400).send('aluno não encontrado!');
+        console.log(aluno);
+
+         return response.json(aluno);
+    }catch(err){
+        return response.status(400).send('Não foi possível atualizar o registro,tente novamente!');
+    }
+        /*
+        
+
+        let query = {$set:{}};
+        for(let i in request.body ){
+            //se nesse índice existe alg propriedade dentro desse objeto e ela for diferente da que vem na requisição, faz o update.
+            if(aluno[i] && aluno[i] !== request.body[i])
+                query.$set[i] = request.body[i];
+            
+
+           const updtAluno = await Aluno.updateOne({_id:request.params._id}, query);
+            return response.send(aluno);     
+        */
+        }
+
+        
+
+
+    }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -10,6 +10,7 @@ routes.get('/alunos', AlunoController.index );//usarei para listar alunos
 routes.post('/alunos', AlunoController.store);//usarei para criar alunos
 routes.delete('/alunos/:_id', AlunoController.delete);
 routes.put('/alunos/:_id', AlunoController.update);
+routes.patch('/alunos/:_id', AlunoController.parcialUpdate);
 
 //<<<<<<<<<<<ROTAS DE USUÁRIOS>>>>>>>>>>>>
 
